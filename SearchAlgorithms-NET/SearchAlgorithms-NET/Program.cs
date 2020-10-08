900249768f21bb61c07d3981cd6287f0d8f67445
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-08 23:51:00 +0300

    Add array input & output

--- SearchAlgorithms-NET/SearchAlgorithms-NET/Program.cs
+++ SearchAlgorithms-NET/SearchAlgorithms-NET/Program.cs
@@ -10,6 +10,24 @@ namespace SearchAlgorithms_NET
     {
         static void Main(string[] args)
         {
+            Console.Write("Array: ");
+            int[] array = new int[] { 5, 10, 1, 7, 4, 9, 8, 6, 2, 3, 5 };
+            Print(array);
+
+            Console.ReadLine();
+        }
+
+        public static void Print<T>(T[] array)
+        {
+            if (array.Length > 0)
+            {
+                Console.Write(array[0]);
+                for (int i = 1; i < array.Length; i++)
+                {
+                    Console.Write(" " + array[i]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
